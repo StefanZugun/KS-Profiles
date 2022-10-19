48482bf6b214197fa9fdec4b57c9dffbe3c620e7
Stefan Zugun <stefan.zugun@bath.edu>
2022-10-19 19:44:15 +0300

    Testing to see if GIT works

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World, what;s app</div>;
+  return <div className="App">Test to see if GIT works</div>;
 }
 
 export default App;
