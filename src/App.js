5d1ed3aa422802c1b8d2ce72471d042b86a60d6d
Stefan Zugun <stefan.zugun@bath.edu>
2022-10-19 20:00:52 +0300

    DUMMY_PROFILES created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
 import "./App.css";
 
+const DUMMY_PROFILES = [
+  {
+    name: "Stefan Zugun",
+    location: "Romania",
+    skills: ["JavaScript", "React"],
+    xp: 10,
+    position: "Senior Developer",
+    interests: ["Full Stack"],
+    availability: "now",
+    languages: ["romanian", "english"],
+  },
+  {
+    name: "Andre Drouge",
+    location: "Sweden",
+    skills: ["JavaScript", "React", "CSS"],
+    xp: 1,
+    position: "Junior Developer",
+    interests: ["Dota"],
+    availability: "July",
+    languages: ["swedish", "english"],
+  },
+  {
+    name: "Cristian Alupoaie",
+    location: "Romania",
+    skills: ["Python"],
+    xp: 5,
+    position: "Mid-level Developer",
+    interests: ["Fishing"],
+    availability: "December",
+    languages: ["romanian"],
+  },
+];
+
+for (const profile of DUMMY_PROFILES) {
+  console.log(profile.name);
+}
+
 function App() {
   return <div className="App">Test to see if GIT works</div>;
 }
